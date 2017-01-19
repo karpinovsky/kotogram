903c9747c2362e1b42c7465641ab9535794a98e0
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-01-19 22:03:30 +0300

    add simple form search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.search(params[:login])
+  end
+
   def show
     @user = User.find_by(login: params[:login])
   end
 
   def feed
+  end
+
+  private
+
+  def search_params
+    params.require(:user).permit(:login)
   end
 end
