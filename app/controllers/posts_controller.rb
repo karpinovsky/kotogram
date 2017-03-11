dba52ef7de93871e3f31cd3e08d568df0e20ee0d
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-11 12:24:26 +0300

    find post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.create!(post_params)
     redirect_to current_user
