f1bf4ce73b4a2bcf80a97db6633b4e7c7b9422f6
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-16 20:14:44 +0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  #devise_for :users, path: '', path_names: { sign_in: '', sign_out: 'signout'}
-  devise_for :users, :controllers => { omniauth_callbacks:  'users/omniauth_callbacks' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' },
+    :controllers => { omniauth_callbacks:  'users/omniauth_callbacks' }
   devise_scope :user do
     authenticated :user do
       root 'users#home', as: :authenticated_root
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
       root 'devise/registrations#new', as: :non_authenticated_root
     end
   end
-  resources :users, param: :username, path: '', except: [ :index, :create, :new ] do
+  resources :users, param: :username, path: '', except: [ :create, :new ] do
     resources :avatars, only: [ :create, :destroy, :update ], path: '/a'
-    resources :posts, only: [ :show, :create, :destroy ], path: '/p' do
+    resources :posts, only: [ :show, :create, :destroy ] do
       resources :images, only: [ :create, :destroy ]
       resources :comments, only: [ :create, :destroy ]
     end
@@ -22,5 +22,5 @@ Rails.application.routes.draw do
     end
   end
   resources :relationships, only: [ :create, :destroy ]
-  get '/p/explore/tags/:name', to: 'posts#tags', as: :tag
+  get '/posts/tags/:name', to: 'posts#tags', as: :tag
 end
