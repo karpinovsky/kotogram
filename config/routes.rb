d37cf57e18458bdba1e3f9323a613c2c7b37f689
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-16 18:13:38 +0300

    remove redefined confirmation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   #devise_for :users, path: '', path_names: { sign_in: '', sign_out: 'signout'}
-  devise_for :users, :controllers => { omniauth_callbacks:  'users/omniauth_callbacks',
-                                       confirmations: 'confirmations'}
+  devise_for :users, :controllers => { omniauth_callbacks:  'users/omniauth_callbacks' }
   devise_scope :user do
     authenticated :user do
       root 'users#home', as: :authenticated_root
