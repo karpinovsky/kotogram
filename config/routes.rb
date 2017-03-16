3c42b680e709e59ccb7997ee06eae94c3530a8ae
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-16 18:11:01 +0300

    change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
   #devise_for :users, path: '', path_names: { sign_in: '', sign_out: 'signout'}
   devise_for :users, :controllers => { omniauth_callbacks:  'users/omniauth_callbacks',
                                        confirmations: 'confirmations'}
+  devise_scope :user do
+    authenticated :user do
+      root 'users#home', as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root 'devise/registrations#new', as: :non_authenticated_root
+    end
+  end
   resources :users, param: :username, path: '', except: [ :index, :create, :new ] do
     resources :avatars, only: [ :create, :destroy, :update ], path: '/a'
     resources :posts, only: [ :show, :create, :destroy ], path: '/p' do
