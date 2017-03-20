a5fa8385a1930552e7193e19487ba298225fefc8
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-20 19:17:57 +0300

    full path for root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' },
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' },
     :controllers => { omniauth_callbacks:  'users/omniauth_callbacks' }
   devise_scope :user do
     authenticated :user do
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
     end
 
     unauthenticated :user do
-      root 'devise/registrations#new', as: :non_authenticated_root
+      #root 'devise/registrations#new', as: :non_authenticated_root
+      root to: redirect('/signup'), as: :non_authenticated_root
     end
   end
   resources :users, param: :username, path: '', except: [ :create, :new ] do
