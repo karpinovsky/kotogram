10484151f57b18edcd758509f7f0d352877d8228
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-23 23:09:52 +0300

    concerns changes

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -5,8 +5,8 @@ module Concerns
       helper_method :current_user?
     end
 
-    def current_user?(user)
-      user == current_user
+    def current_user?(user_by_username)
+      current_user == user_by_username
     end
   end
 end
