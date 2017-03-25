352002ef83c7cf6912e09523721a6c6cf1838510
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-25 20:29:33 +0300

    remove redefined devise registration controller

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,7 +0,0 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  def new
-    build_resource({})
-    resource.build_profile
-    respond_with self.resource
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '',
     path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' },
-    :controllers => { registrations: 'users/registrations',
-                      omniauth_callbacks:  'users/omniauth_callbacks' }
+    :controllers => { omniauth_callbacks:  'users/omniauth_callbacks' }
   devise_scope :user do
     authenticated :user do
       root 'users#home', as: :authenticated_root
