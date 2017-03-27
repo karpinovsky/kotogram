b289fc15d524f769c33c87762a7552ff29580f87
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-28 00:52:50 +0300

    add root path for non authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '',
-    path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' },
-    :controllers => { omniauth_callbacks:  'users/omniauth_callbacks' }
+  devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login',
+                                             sign_out: 'logout', edit: ':username/edit'},
+                     controllers: { omniauth_callbacks:  'users/omniauth_callbacks' }
+
   devise_scope :user do
     authenticated :user do
       root 'users#home', as: :authenticated_root
     end
 
     unauthenticated :user do
-      #root 'devise/registrations#new', as: :non_authenticated_root
-      root to: redirect('/signup'), as: :non_authenticated_root
+      root 'devise/registrations#new', as: :non_authenticated_root
     end
   end
 
-  resources :users, path: '', param: :username, except: [ :create, :new ] do
+  get '/', to: 'users#index', as: :users
+  resources :users, path: '', param: :username, only: [:show] do
     resources :posts, only: [ :show, :create, :destroy ] do
       resources :images, only: [ :create, :destroy ]
       resources :comments, only: [ :create, :destroy ]
     end
-    get 'users', on: :collection, action: :index, as: ''
     member do
       get :following, :followers
     end
