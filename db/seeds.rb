27ff5d5ee2efc9894f6cfeefea122c878a43e9cc
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-28 04:11:29 +0300

    username fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 1.upto(3) do |i|
   user = User.new(
-    email: "user#{i}@kotogram.ru",
+    email: "kotogram#{i}@kotogram.ru",
     password: 'kotogram',
     password_confirmation: 'kotogram',
     confirmed_at: Time.now,
-    username: "user#{i}@kotogram.ru",
-    full_name: "user#{i}",
+    username: "kotogram#{i}",
+    full_name: "kotogram#{i}",
     about_me: "Hi, I'am #{i} user"
   )
 
