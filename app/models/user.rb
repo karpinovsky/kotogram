a88c6d19e0d33d1745401e579fb577cfc73d12c4
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-28 04:12:21 +0300

    relationships between users

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,13 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
+  has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: 'followed_id',
+                                   class_name: 'Relationship',
+                                   dependent: :destroy
+  has_many :followers, through: :reverse_relationships
+
   def to_param
     username
   end
