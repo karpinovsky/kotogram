ea597c693d8abdd33e4427f24c23dc15ee41bfe1
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-31 07:07:58 +0300

    change comments commenter type, use reversible

--- db/migrate/20170328213807_change_integer_type_for_commenter.rb
+++
@@ -1,9 +0,0 @@
-class ChangeIntegerTypeForCommenter < ActiveRecord::Migration[5.0]
-  def up
-    change_column :comments, :commenter, :string
-  end
-
-  def down
-    change_column :comments, :commenter, :integer
-  end
-end
