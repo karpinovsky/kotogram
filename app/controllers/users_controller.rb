d7ffdaee06490d4f249d521a1d4a291da1200ffc
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-03-31 12:03:24 +0300

    show feeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def home
-    #@feed_items = current_user.feed.order('created_at DESC')
+    @feed_items = current_user.feed.order('created_at DESC')
   end
 
   def index
