be908c0cfa91e8cac34662b523a7bc27ac0d474b
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-04-03 07:47:02 +0300

    user could delete his comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,9 +6,14 @@ class CommentsController < ApplicationController
     redirect_back(fallback_location: authenticated_root_path)
   end
 
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_back(fallback_location: authenticated_root_path)
+  end
+
   private
 
-  def comment_params
-    params.require(:comment).permit(:post_id, :commenter, :body)
-  end
+    def comment_params
+      params.require(:comment).permit(:post_id, :commenter, :body)
+    end
 end
