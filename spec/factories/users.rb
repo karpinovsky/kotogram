ea190173b53b1d9848c432e6f3ecdc694d5a9ae2
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-05-15 23:56:38 +0300

    tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,18 @@
 FactoryGirl.define do
   factory :user do
     email                 'email@kotogram.ru'
-    name                  'name'
-    username              'username'
     password              'password'
     password_confirmation 'password'
+
+    factory :user_with_profile do
+      after(:create) do |user|
+        create(:profile, user: user)
+      end
+    end
+  end
+
+  factory :profile do
+    username 'username'
+    full_name 'fullname'
   end
 end
