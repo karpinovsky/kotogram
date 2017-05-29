80db6530ff34675af60d168afc80f189d19fe79a
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-05-29 22:25:30 +0300

    changes

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+set :rails_env, :staging
+set :stage, :staging
 server "staging.kotogram.com", user: "kotogram", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
