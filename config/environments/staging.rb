6cd45d25cc79ab54c551db26b6a1a353497725de
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-06-03 20:32:20 +0300

    fixed errors with mailgun

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,9 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { :host => 'kotogram-project.com' }
   config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.mailgun_settings = {
-    api_key: '48826bd31af47a4d3ddf71f2af566b8c656c9921ed12c848e5dac0734640b49f594d411a783d3facc4a6ec559afb32356b56f9859d64901b5fe8551b70b1ba46',
-    domain: 'mail.kotogram-project.com',
+    api_key: 'key-48e15945ae08563021d2f6bae7c24c86',
+    domain: 'kotogram-project.com',
   }
 end
