a6a2ab3019d6c0843e02bff33cb9d80a4d7eee32
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-06-07 23:19:09 +0300

    add 500 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, :with => :render_500
 
   protected
 
@@ -14,6 +15,14 @@ class ApplicationController < ActionController::Base
   def render_404
     respond_to do |format|
       format.html { render :file => "#{Rails.root}/public/404", :layout => true, :status => :not_found }
+      format.xml  { head :not_found }
+      format.any  { head :not_found }
+    end
+  end
+
+  def render_500
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/500", :layout => true, :status => :not_found }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
