d193fb08d3c228947d8b03af3088faa0d1107dea
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-06-10 18:24:34 +0300

    remove unnecessary views, actions, routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,12 @@
 class UsersController < ApplicationController
   include Concerns::UserByProfileUsername
   skip_before_action :authenticate_user!, only: [:show]
-  respond_to :html, :js
 
   def home
     @feed_items = current_user.feed.order('created_at DESC')
   end
 
-  def index
-    @users = User.joins(:profile).where('username LIKE ? ', "%#{params[:search]}%")
-  end
-
   def show
     render "users/show/show"
-  end
-
-  def following
-  end
-
-  def followers
   end
 end
