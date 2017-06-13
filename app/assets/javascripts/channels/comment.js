32f089c36e28c3583c221565901d357134c4fe59
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-06-13 15:06:33 +0300

    create and configure comments channel

--- app/assets/javascripts/channels/comment.js
+++ app/assets/javascripts/channels/comment.js
@@ -8,6 +8,23 @@ App.comment = App.cable.subscriptions.create("CommentChannel", {
   },
 
   received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
+    if (data){
+      var li = document.createElement("LI");
+      li.className = "comment";
+
+      var text = document.createTextNode(data.comment.body);
+      var span = document.createElement("SPAN");
+      span.appendChild(text);
+
+      var a = document.createElement("A");
+      a.className = "comment-user-username-link";
+      a.href = "/" + data.comment.user_username;
+      a.innerHTML = data.comment.user_username;
+
+      li.appendChild(a);
+      li.appendChild(span);
+      document.getElementsByClassName("post-" + data.comment.post_id)[0].appendChild(li);
+      document.getElementsByClassName("post-form-comment-input-" + data.comment.post_id)[0].value = "";
+    }
   }
 });
--- app/channels/comment_channel.rb
+++ app/channels/comment_channel.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class CommentChannel < ApplicationCable::Channel
   def subscribed
+    stream_from "comment_channel"
     # stream_from "some_channel"
   end
 
