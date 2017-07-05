4d3b311159f2597c44e987fa4050e9bf7e75110f
Kirill Karpinovsky <karpinovsky@gmail.com>
2017-07-05 18:41:12 +0300

    Configure devise omniauth via Facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,7 +247,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+  config.omniauth :facebook, ENV['KOTOGRAM_FACEBOOK_APP_ID'], ENV['KOTOGRAM_FACEBOOK_APP_SECRET'],
     scope: 'email, public_profile', info_fields: 'email, first_name, last_name',
     secure_image_url: true, image_size: { width: '150', height: '150' }
 
